740ac25dfe0dae552ddcdf7daaf7025a2e859abb
rei <h.watanabe360@gmail.com>
2025-01-01 15:45:24 +0900

    feat: membershipページにユーザータイプ選択コンポーネントを追加

---
+++ src/pages/membership/MembershipPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+import MemberShipPageUserTypeSelect from './MemberShipPageUserTypeSelect.vue';
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <MemberShipPageUserTypeSelect />
+    <div>route.query.user_type: {{ route.query.user_type }}</div>
+  </div>
+</template>
