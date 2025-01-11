ee832c8c096ae712f14dff4c72489215206d8ac7
rei <h.watanabe360@gmail.com>
2025-01-12 01:29:36 +0900

    feat: gen-api の実行時に prettier が実行されるようにし、それに伴い docker から npm に移行

--- scripts/generate-apis.ts
+++ scripts/generate-apis.ts
@@ -2,7 +2,6 @@ import fs from 'fs/promises'
 import path from 'path'
 
 import { exec } from 'child_process'
-import { promisify } from 'util'
 import { addApis } from './add-apis'
 import { addTsIgnoreToImports } from './add-ts-ignore-to-imports'
 
@@ -13,32 +12,42 @@ const SWAGGER_PATH =
 const GENERATED_DIR = 'src/lib/apis/generated'
 
 const generateCmd = [
-  'docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:v7.5.0',
+  'npx',
+  'openapi-generator-cli',
   'generate',
   '-g',
   'typescript-axios',
   '-i',
   SWAGGER_PATH,
   '-o',
-  `/local/${GENERATED_DIR}`,
+  GENERATED_DIR,
+  '--enable-post-process-file',
 ]
 
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
 
-const execPromise = promisify(exec)
-
 ;(async () => {
   await fs.mkdir(path.resolve(__dirname, '../', GENERATED_DIR), {
     recursive: true,
   })
 
-  const { stdout, stderr } = await execPromise(generateCmd.join(' '))
-  console.log(stdout)
-  if (stderr) {
-    console.error(stderr)
-  }
+  exec(
+    generateCmd.join(' '),
+    { env: { ...process.env, TS_POST_PROCESS_FILE: 'prettier --write' } },
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(error.message)
+        return
+      }
+      if (stderr) {
+        console.error(stderr)
+        return
+      }
+      console.log(stdout)
+    },
+  )
 
   // generate Apis class
   await addApis(GENERATED_DIR)
