6f995a25258e3784f4418c98c937904e36b7fd5d
rei <h.watanabe360@gmail.com>
2025-04-01 17:03:54 +0900

    fix: `process.env.NODE_ENV`から`import.meta.env.DEV`に変更

--- src/lib/msw/msw.ts
+++ src/lib/msw/msw.ts
@@ -6,7 +6,7 @@ import { listHandlers } from './handlers/list'
 const handlers = [adminHandlers, customerHandlers, invoiceHandlers, listHandlers].flat()
 
 export const initMock = async () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (import.meta.env.DEV) {
     const { setupWorker } = await import('msw/browser')
     const worker = setupWorker(...handlers)
     worker.start({
