2b8fd01bcf8206381eab1b29e899bdd9daa7bac9
rei <107461411+reiroop@users.noreply.github.com>
2026-05-02 07:30:37 +0900

    chore: `pnpm create nuxt@latest`

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@nuxt/icon',
+    '@vueuse/nuxt'
+  ]
+})
--- src/lib/apis/index.ts
+++
@@ -1,6 +0,0 @@
-import { Apis, Configuration } from './generated'
-
-const apis = new Apis(new Configuration({ basePath: '/api' }))
-
-export default apis
-export * from './generated'
